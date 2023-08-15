74da0350eaf30ae9b8170e18ad972e830cc0fd71
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-08-15 18:48:52 +0300

    fix error

--- js/main.js
+++ js/main.js
@@ -8,13 +8,6 @@ form.addEventListener('focusout', handleBlur);
 textarea.addEventListener('input', handleCounter);
 let placeholder = null;
 
-let span = document.querySelector('#errorName');
-console.log(span);
-
-setTimeout(() => {
-    nameInput.focus();
-}, 1000);
-
 //todo - функция принимает ноду и возвращает данные для отправки на сервер
 function serializeForm(formNode) {
     const { elements } = formNode;
@@ -39,6 +32,7 @@ function handleFocus(e) {
     let input = e.target;
     placeholder = input.placeholder;
     input.placeholder = '';
+    removeError(e.target);
 }
 
 function handleBlur(e) {
@@ -68,20 +62,18 @@ function addErrorClass(element) {
 function addAlertMessage(element) {
     addErrorClass(element);
     let name = element.name;
-
-    let span = document.querySelector('#errorName');
-    console.log(span);
+    let selector = `${'.error' + name[0]?.toUpperCase() + name?.slice(1)}`;
+    let spanError = document.querySelector(selector);
+    spanError?.classList.add('error');
 }
 
-//todo - функция удаления данных об ошибки при каждом новом событии все затирается
+//todo - функция удаления данных об ошибке при каждом новом событии все затирается
 function removeError(element) {
+    element.parentNode.classList.remove('error');
     let name = element.name;
-    let parent = element.parentNode;
-    let span = document.querySelector(
-        `${'#error' + name[0]?.toUpperCase() + name?.slice(1)}`
-    );
-    parent.classList.remove('error');
-    span?.remove();
+    let selector = `${'.error' + name[0]?.toUpperCase() + name?.slice(1)}`;
+    let spanError = document.querySelector(selector);
+    spanError?.classList.remove('error');
 }
 
 //todo - функция валидации
@@ -90,8 +82,11 @@ function validate(formNode) {
     for (let index = 0; index < formNode.length; index++) {
         const input = formNode[index];
         removeError(input);
-        if (input.name === 'name' && input.value === '') {
+        if (!input.value.trim() && input.type !== 'checkbox') {
             addAlertMessage(input);
+        }
+        if (input.type === 'checkbox' && !input.checked) {
+            addErrorClass(input);
         }
     }
     return result;
