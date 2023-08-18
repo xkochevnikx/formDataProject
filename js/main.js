ce87015da102454dffda6179c7df168221d42475
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-08-18 15:27:07 +0300

    add custom select

--- js/main.js
+++ js/main.js
@@ -4,20 +4,27 @@ let textarea = document.querySelector('.form__item-textarea');
 let successButton = document.querySelector('.success_box');
 let formWrapper = document.querySelector('.form_wrapper');
 let nameInput = document.form.name;
+let select = document.querySelector('.select_header');
 form.addEventListener('submit', handleSubmit);
 form.addEventListener('focusin', handleFocus);
 form.addEventListener('focusout', handleBlur);
 textarea.addEventListener('input', handleCounter);
 successButton.addEventListener('click', handleCloseSuccess);
+select.addEventListener('click', openSelect);
 let placeholder = null;
 
 //todo - функция принимает ноду и возвращает данные для отправки на сервер
 function serializeForm(formNode) {
-    console.log(formNode);
     const data = [];
+    data.push({ [textarea.name]: textarea.value });
     Array.from(formNode).map((element) => {
-        const { name, value } = element;
-        data.push({ [name]: value });
+        if (element.type !== 'checkbox') {
+            const { name, value } = element;
+            data.push({ [name]: value });
+        } else {
+            const { name, checked } = element;
+            data.push({ [name]: checked });
+        }
     });
     return data;
 }
@@ -81,8 +88,8 @@ function removeError(element) {
 //todo - функция валидации
 function validate(formNode) {
     let result = false;
-    for (let index = 0; index < formNode.length; index++) {
-        const input = formNode[index];
+    for (let node of formNode) {
+        const input = node;
         removeError(input);
         if (input.value === '' && input.type !== 'checkbox') {
             result = true;
@@ -114,14 +121,31 @@ function handleCloseSuccess() {
     formWrapper.style.display = 'flex';
 }
 
+//todo - открыть селект
+function openSelect() {
+    document.querySelector('.select_body').classList.toggle('isActiv');
+    document.querySelectorAll('.select_item').forEach((select) => {
+        select.addEventListener('click', function () {
+            let content = this.innerText.trim();
+            let value = this.dataset.value;
+            document.querySelector('.current_select').innerText = content;
+            document
+                .querySelector('.select_header')
+                .setAttribute('data-value', value);
+            document.querySelector('.select_body').classList.remove('isActiv');
+        });
+    });
+}
+
 //todo основной обработчик события
 async function handleSubmit(e) {
     e.preventDefault();
     let inputs = document.querySelectorAll('.required');
+
     if (!validate(inputs)) {
         let data = serializeForm(inputs);
         form.reset();
-        textarea.value = '';
+        counter.innerText = '1000';
         loaderFn();
         let response = await fetchUsers(data);
         if (response.ok) {
