c90659feebfb633eeaf4f89ef424574e1132364e
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-08-23 12:04:43 +0300

    release version 0.3

--- js/main.js
+++ js/main.js
@@ -2,66 +2,89 @@ let formWrapper = document.querySelector('.form-wrapper');
 let successBox = document.querySelector('.success-box');
 let loaderBox = document.querySelector('.loader-box');
 let form = formWrapper.querySelector('.form');
+let button = form.querySelector('.form__button');
 let counter = form.querySelector('.form__counter');
+let inputs = form.querySelectorAll('.required');
+let textarea = form.querySelector('.form__textaria');
 form.addEventListener('submit', handleSubmit);
-form.addEventListener('focusin', handleFocus);
-form.addEventListener('focusout', handleBlur);
+form.addEventListener('input', handleInput);
 successBox.addEventListener('click', handleCloseSuccess);
 
-//todo основной обработчик события
+function submitOpen() {
+    let result = Array.from(inputs).filter(
+        (elem) => elem.dataset.valid === 'true'
+    );
+    if (result.length > 4) {
+        button.removeAttribute('disabled');
+        button.classList.add('success');
+    }
+}
+
+function handleInput(e) {
+    if (e.target.classList.contains('required')) {
+        inputCheck(e.target);
+        submitOpen();
+    }
+}
+
+function inputCheck(element) {
+    console.log(element);
+    if (
+        !element.classList.contains('required-select') &&
+        element.type !== 'checkbox'
+    ) {
+        let value = element.value.trim();
+        let regExp = new RegExp(element.getAttribute('data-reg'));
+
+        if (regExp.test(value)) {
+            element.classList.remove('error');
+            element.classList.add('success');
+            element.setAttribute('data-valid', true);
+        }
+        if (!regExp.test(value)) {
+            element.classList.remove('success');
+            element.classList.add('error');
+            element.setAttribute('data-valid', false);
+        }
+    } else {
+        element.classList.add('success');
+        element.setAttribute('data-valid', true);
+    }
+    if (element.classList.contains('rules__input') && element.checked) {
+        console.log(element);
+        element.setAttribute('data-valid', true);
+    }
+}
+
 async function handleSubmit(e) {
     e.preventDefault();
 
-    let inputs = form.querySelectorAll('.required');
-    console.log(inputs);
-
-    // if (!validate(inputs)) {
-    //     let data = serializeForm(inputs);
-    //     loaderFn();
-    //     let response = await fetchUsers(data);
-    //     if (response.ok) {
-    //         form.reset();
-    //         selectReset();
-    //         counter.innerText = '1000';
-    //         successFetch();
-    //     }
-    // }
+    let data = serializeForm(inputs);
+    loaderFn();
+    let response = await fetchUsers(data);
+    if (response.ok) {
+        form.reset();
+        // counter.innerText = '1000';
+        successFetch();
+    }
 }
 
-// //todo - функция валидации
-// function validate(formNode) {
-//     let result = false;
-//     Array.from(formNode).forEach((node) => {
-//         const input = node;
-//         toggleError(input);
-//     });
-//     return result;
-// }
+function serializeForm(formNode) {
+    const data = [];
 
-//todo - функция принимает ноду и возвращает данные для отправки на сервер
-// function serializeForm(formNode) {
-//     const data = [];
+    Array.from(formNode).forEach((element) => {
+        if (element.classList.contains('rules__input')) {
+            data.push({ [element.dataset.name]: element.checked });
+        } else {
+            data.push({ [element.name]: element.value });
+        }
+    });
 
-//     Array.from(formNode).forEach((element) => {
-//         if (element.closest('.rules__input')) {
-//             data.push({ [element.dataset.name]: element.checked });
-//         }
+    data.push({ [textarea.dataset.name]: textarea.value });
 
-//         if (element.closest('.form-input')) {
-//             data.push({ [element.dataset.name]: element.value });
-//         }
+    return data;
+}
 
-//         if (element.closest('.select-header')) {
-//             data.push({ [element.dataset.name]: element.dataset.value });
-//         }
-//     });
-
-//     data.push({ [textarea.dataset.name]: textarea.value });
-
-//     return data;
-// }
-
-//todo - функция отправки данных на сервер
 async function fetchUsers(data) {
     const response = await fetch('http://localhost:3000/users', {
         method: 'POST',
@@ -73,40 +96,21 @@ async function fetchUsers(data) {
     return response;
 }
 
-//todo функция для отображения счетчика поля комментариев
 function handleCounter() {
     let count = textarea.maxLength - textarea.value.length;
-    counter.innerText = `${count}`;
+    counter.innerText = `${count}/1000`;
 }
 
-//todo - обработка фокуса
-function handleFocus(e) {
-    // toggleError(e.target);
-}
-
-//todo - обработка выхода из фокуса
-function handleBlur(e) {}
-
-//todo - функция переключения данных об ошибке
-function toggleError(element) {
-    let error = element.closest('.fieldset:invalid');
-    if (error) {
-    }
-}
-
-//todo - функция добавления лоадера на страницу
 function loaderFn() {
     formWrapper.classList.remove('active');
     loaderBox.classList.add('active');
 }
 
-//todo - запрос успешно отправлен
 function successFetch() {
     loaderBox.classList.remove('active');
     successBox.classList.add('active');
 }
 
-//todo - закрываем окно с информацией об успешной отправке данных
 function handleCloseSuccess() {
     successBox.classList.remove('active');
     formWrapper.classList.add('active');
